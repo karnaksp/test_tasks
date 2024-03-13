17e5dd895ca15657c75e2fe722d4873c79dc2aa8
Denis Irinyakov <irinyakov2016@yandex.ru>
2024-03-13 19:17:08 +0700

    1

--- rightscan/simple_ui/Конфигурация Птицы/simple_warehouse_handlers.py
+++ rightscan/simple_ui/Конфигурация Птицы/simple_warehouse_handlers.py
@@ -9,6 +9,25 @@ from sqlite3.dbapi2 import Error
 from datetime import datetime
 from PIL import Image
 
+from tokenize import Double
+from pony import orm
+from pony.orm import Database, Required, Set, Json, PrimaryKey, Optional
+from pony.orm.core import db_session
+import datetime
+
+db = Database()
+db.bind(
+    provider="sqlite",
+    filename="//data/data/ru.travelfood.simple_ui/databases/SimpleBirds",
+    create_db=True,
+)
+
+
+class SW_Birds(db.Entity):
+    name = Required(str)
+    color_feathers = Optional(str)
+    image_path = Optional(str)
+
 
 unit_id = -1
 nom_id = -1
