ff1741a52326d5c4b3b520da3f5cddc107884ef5
Denis Irinyakov <irinyakov2016@yandex.ru>
2024-03-14 14:34:09 +0700

    reright birds4

--- rightscan/simple_ui/Конфигурация Птицы/birds_handlers.py
+++ rightscan/simple_ui/Конфигурация Птицы/birds_handlers.py
@@ -7,7 +7,6 @@ import sqlite3  # This is another way to work with SQL
 from sqlite3.dbapi2 import Error
 from datetime import datetime
 from PIL import Image
-
 from tokenize import Double
 from pony import orm
 from pony.orm import Database, Required, Set, Json, PrimaryKey, Optional
@@ -17,15 +16,61 @@ import datetime
 db = Database()
 db.bind(
     provider="sqlite",
-    filename="//data/data/ru.travelfood.simple_ui/databases/SimpleBirds",
+    filename="//data/data/ru.travelfood.simple_ui/databases/Simple_Birds_2",
     create_db=True,
 )
 
 
-class SW_Birds(db.Entity):
+class SW_Units(db.Entity):
     name = Required(str)
-    color_feathers = Optional(str)
+
+
+class SW_Groups(db.Entity):
+    name = Required(str)
+
+
+class SW_Cells(db.Entity):
+    name = Required(str)
+    barcode = Optional(str)
+
+
+class SW_Goods(db.Entity):
+    name = Required(str)
+    product_number = Optional(str)
+    barcode = Optional(str)
+    group = Optional(str)
+    unique = Optional(int)
+    unit = Required(str)
+    price = Optional(float)
     pictures = Optional(Json)
+    created_at = Optional(datetime.datetime, sql_default="CURRENT_TIMESTAMP")
+
+
+class SW_Account(db.Entity):
+    sku = Required(int)
+    cell = Required(int)
+    qty = Required(float)
+
+    period = Optional(datetime.datetime, sql_default="CURRENT_TIMESTAMP")
+
+
+class SW_Inventory(db.Entity):
+    description = Required(str)
+    created_at = Optional(datetime.datetime, sql_default="CURRENT_TIMESTAMP")
+    lines = Set("SW_Inventory_line")
+
+
+class SW_Inventory_line(db.Entity):
+    sku = Required(int)
+    cell = Required(int)
+    qty = Required(float)
+    inventory = Required("SW_Inventory")
+
+    date = Optional(datetime.datetime, sql_default="CURRENT_TIMESTAMP")
+
+
+def init():
+    db.generate_mapping(create_tables=True)
 
 
 unit_id = -1
@@ -38,9 +83,19 @@ green_size = 0
 yellow_size = 0
 
 
+def init_on_start(hashMap, _files=None, _data=None):
+    init()
+    return hashMap
+
+
 def menu_input(hashMap, _files=None, _data=None):
-    if hashMap.get("listener") == "btn_add":
-        hashMap.put("ShowScreen", "ПтицыЗапись")
+    if hashMap.get("listener") == "menu":
+        if hashMap.get("menu") == "Список птиц":
+            hashMap.put("ShowScreen", "Список птиц")
+        if hashMap.get("menu") == "Карточка птицы":
+            hashMap.put("ShowScreen", "Карточка птицы")
+        elif hashMap.get("menu") == "Создание новой птицы":
+            hashMap.put("ShowScreen", "Создание новой птицы")
     elif hashMap.get("listener") == "ON_BACK_PRESSED":
         hashMap.put("FinishProcess", "")
     return hashMap
